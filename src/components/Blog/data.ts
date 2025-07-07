ebf3c9762a188ecb4909edf58ec5f6ea1bba42a9
elbaz-youssef <youssef.devworks@gmail.com>
2025-07-07 18:25:19 +0100

    fix images in the Blog Page

--- src/components/Blog/data.ts
+++ src/components/Blog/data.ts
@@ -1,36 +1,41 @@
+import postImg1 from "@/assets/blog/b1.jpg";
+import postImg2 from "@/assets/blog/b2.jpg";
+import postImg3 from "@/assets/blog/b3.jpg";
+import postImg4 from "@/assets/blog/b4.jpg";
+import postImg5 from "@/assets/blog/b6.jpg";
 
 export const posts = [
     {
         numberImg: '13/01',
-        srcImg: 'src/assets/blog/b1.jpg',
+        srcImg: postImg1,
         title: 'The Cotton-Jersey Zip-Up Hoodie',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, adipisci quis officia molestiae ad, dicta quisquam fugit eaque repudiandae illo fuga praesentium. Quibusdam error laboriosam unde nobis natus magnam? Quae.',
         link: 'Continue Reading'
     },
     {
         numberImg: '13/04',
-        srcImg: 'src/assets/blog/b2.jpg',
+        srcImg: postImg2,
         title: 'How To Style a Quiff',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, adipisci quis officia molestiae ad, dicta quisquam fugit eaque repudiandae illo fuga praesentium. Quibusdam error laboriosam unde nobis natus magnam? Quae.',
         link: 'Continue Reading'
     },
     {
         numberImg: '12/01',
-        srcImg: 'src/assets/blog/b3.jpg',
+        srcImg: postImg3,
         title: 'Must-Have Skater Girl Items',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, adipisci quis officia molestiae ad, dicta quisquam fugit eaque repudiandae illo fuga praesentium. Quibusdam error laboriosam unde nobis natus magnam? Quae.',
         link: 'Continue Reading'
     },
     {
         numberImg: '16/01',
-        srcImg: 'src/assets/blog/b4.jpg',
+        srcImg: postImg4,
         title: 'Runway Inspired Trends',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, adipisci quis officia molestiae ad, dicta quisquam fugit eaque repudiandae illo fuga praesentium. Quibusdam error laboriosam unde nobis natus magnam? Quae.',
         link: 'Continue Reading'
     },
     {
         numberImg: '10/03',
-        srcImg: 'src/assets/blog/b6.jpg',
+        srcImg: postImg5,
         title: 'AW20 Menswear Trends',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, adipisci quis officia molestiae ad, dicta quisquam fugit eaque repudiandae illo fuga praesentium. Quibusdam error laboriosam unde nobis natus magnam? Quae.',
         link: 'Continue Reading'
